aa7304e55b604aca6ddf18b4c163dbf3e69dfb88
Kyle Coberly <kyle.coberly@gmail.com>
2019-04-27 08:20:34 -0600

    Solution added

---
+++ app.js
@@ -0,0 +1,23 @@
+function addWordsToMadLib(){
+    var word1 = document.querySelector("#word-1-input").value;
+    var word2 = document.querySelector("#word-2-input").value;
+    var word3 = document.querySelector("#word-3-input").value;
+    var word4 = document.querySelector("#word-4-input").value;
+    var word5 = document.querySelector("#word-5-input").value;
+    var word6 = document.querySelector("#word-6-input").value;
+    var word7 = document.querySelector("#word-7-input").value;
+    var word8 = document.querySelector("#word-8-input").value;
+    var word9 = document.querySelector("#word-9-input").value;
+    var word10 = document.querySelector("#word-10-input").value;
+
+    document.querySelector("#word-1").textContent = word1;
+    document.querySelector("#word-2").textContent = word2;
+    document.querySelector("#word-3").textContent = word3;
+    document.querySelector("#word-4").textContent = word4;
+    document.querySelector("#word-5").textContent = word5;
+    document.querySelector("#word-6").textContent = word6;
+    document.querySelector("#word-7").textContent = word7;
+    document.querySelector("#word-8").textContent = word8;
+    document.querySelector("#word-9").textContent = word9;
+    document.querySelector("#word-10").textContent = word10;
+}
